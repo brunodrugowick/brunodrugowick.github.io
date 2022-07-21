69a7f7ad16d16447fad8ac3c5091dda3e680d648
Bruno Drugowick <brunodrugowick@users.noreply.github.com>
2022-07-21 14:41:34 -0300

    Trying to fix again

--- scripts/check_for_changes_and_commit.sh
+++ scripts/check_for_changes_and_commit.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-git config --global user.name 'Bruno Drugowick'
-git config --global user.email 'brunodrugowick@users.noreply.github.com'
-
 if git diff-index --quiet HEAD; then
 	echo "No changes detected, nothing will be done."
-	exit 0
 else
+  git config --global user.name 'Bruno Drugowick'
+  git config --global user.email 'brunodrugowick@users.noreply.github.com'
 	echo "New posts found, let's commit them and push."
 	date >last_updated_date
 	git add .
